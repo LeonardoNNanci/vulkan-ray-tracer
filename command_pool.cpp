0f5ffd389339fc8f2da10808c06a00ae64e6b599
LeonardoNNanci <leonardonanci@id.uff.br>
2024-01-06 14:47:58 -0300

    refactor command pool

---
+++ command_pool.cpp
@@ -0,0 +1,95 @@
+#define VULKAN_HPP_NO_CONSTRUCTORS
+
+#include "command_pool.hpp"
+
+CommandPoolBuilder::CommandPoolBuilder(std::shared_ptr<Setup> setup) : IHasSetup(setup) {}
+
+std::shared_ptr<CommandPool> CommandPoolBuilder::build()
+{
+    this->commandPool = std::make_shared<CommandPool>(this->setup);
+
+    vk::CommandPoolCreateInfo commandPoolInfo{
+        .flags = vk::CommandPoolCreateFlagBits::eResetCommandBuffer,
+        .queueFamilyIndex = this->setup->graphicsQueue.familyIndex
+    };
+
+    this->commandPool->handle = this->setup->device.createCommandPool(commandPoolInfo);
+    this->commandPool->queue = this->setup->graphicsQueue;
+
+    return this->commandPool;
+}
+
+CommandPool::CommandPool(std::shared_ptr<Setup> setup) : IHasSetup(setup) {}
+
+std::shared_ptr<CommandBuffer> CommandPool::createCommandBuffer(vk::CommandBufferUsageFlags usage)
+{
+    auto commandBuffer = std::make_shared<CommandBuffer>(this->setup);
+
+    vk::CommandBufferAllocateInfo allocInfo{
+        .commandPool = this->handle,
+        .level = vk::CommandBufferLevel::ePrimary,
+        .commandBufferCount = 1
+    };
+    commandBuffer->handle = this->setup->device.allocateCommandBuffers(allocInfo)[0];
+    commandBuffer->usage = usage;
+    commandBuffer->queue = this->queue;
+
+    return commandBuffer;
+}
+
+CommandPool::~CommandPool() {
+    this->setup->device.destroyCommandPool(this->handle);
+}
+
+CommandBuffer::CommandBuffer(std::shared_ptr<Setup> setup) : IHasSetup(setup) {}
+
+void CommandBuffer::begin() {
+    vk::CommandBufferBeginInfo beginInfo{
+        .flags = this->usage
+    };
+    this->handle.begin(beginInfo);
+}
+
+void CommandBuffer::setWaitSemaphore(vk::Semaphore semaphore) {
+    this->waitSemaphores.push_back(semaphore);
+}
+
+void CommandBuffer::setSignalSemaphore(vk::Semaphore semaphore) {
+    this->signalSemaphores.push_back(semaphore);
+}
+
+void CommandBuffer::setFence() {
+    this->destroyFence();
+    this->fence = this->setup->device.createFence({});
+}
+
+void CommandBuffer::resetFence() {
+    this->setup->device.resetFences({ this->fence });
+}
+
+void CommandBuffer::waitFinished()
+{
+    this->setup->device.waitForFences({ this->fence }, vk::True, UINT64_MAX);
+}
+
+void CommandBuffer::destroyFence() {
+    if (this->fence != VK_NULL_HANDLE)
+        this->setup->device.destroyFence(this->fence);
+}
+
+void CommandBuffer::submit() {
+    this->handle.end();
+
+    vk::SubmitInfo submitInfo{
+        .commandBufferCount = 1,
+        .pCommandBuffers = &this->handle
+    };
+    submitInfo.setWaitSemaphores(this->waitSemaphores);
+    submitInfo.setSignalSemaphores(this->signalSemaphores);
+
+    this->queue.handle.submit(submitInfo, this->fence);
+}
+
+CommandBuffer::~CommandBuffer() {
+    this->destroyFence();
+}
--- command_pool.hpp
+++ command_pool.hpp
@@ -7,21 +7,55 @@
 #include "builder.hpp"
 #include "setup.hpp"
 
-class CommandBuffer {
+class CommandBuffer : private IHasSetup {
 public:
-	void 
-};
+	vk::CommandBuffer handle;
+	Queue queue;
+	vk::CommandBufferUsageFlags usage;
 
-class CommandPool {
-public:
-	CommandBuffer createCommandBuffer();
+	CommandBuffer(std::shared_ptr<Setup> setup);
+
+	void begin();
+
+	void submit();
+
+	void setWaitSemaphore(vk::Semaphore semaphore);
+
+	void setSignalSemaphore(vk::Semaphore semaphore);
+
+	void setFence();
+
+	void resetFence();
+
+	void waitFinished();
+
+	~CommandBuffer();
 
 private:
+	std::vector<vk::Semaphore> waitSemaphores;
+	std::vector<vk::Semaphore> signalSemaphores;
+	vk::Fence fence = VK_NULL_HANDLE;
 
+
+	void destroyFence();
 };
 
-class CommandPoolBuilder : public Builder<std::shared_ptr<CommandPool>> {
+class CommandPool : private IHasSetup {
 public:
+	vk::CommandPool handle;
+	Queue queue;
+
+	CommandPool(std::shared_ptr<Setup> setup);
+
+	std::shared_ptr<CommandBuffer> createCommandBuffer(vk::CommandBufferUsageFlags usage = {});
+
+	~CommandPool();	
+};
+
+class CommandPoolBuilder : public Builder<std::shared_ptr<CommandPool>>, private IHasSetup {
+public:
+	CommandPoolBuilder(std::shared_ptr<Setup> setup);
+
 	std::shared_ptr<CommandPool> build();
 
 private:
